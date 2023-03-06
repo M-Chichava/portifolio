2ffb1aa902887f3878889533861a42cbe160fd76
Marcos Chichava <76664047+M-Chichava@users.noreply.github.com>
2023-03-06 02:02:29 +0200

    Swiper, footer

---
+++ src/components/testimonials/Testimonials.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import './Testimonials.css'
+
+const Testemunial = () => {
+  return(
+    <div className="testimonials section container">
+      
+      <h2 className="section__title">My clients say</h2>
+      <span className="section__subtitle">Testimonial</span>
+
+      <div className="testimonial__container">
+        
+      </div>
+    </div>
+  )
+}
+
+export default Testemunial
